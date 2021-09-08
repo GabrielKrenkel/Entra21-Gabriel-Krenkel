8388915eb9a677ec03d74ce53fb357fe7d70ad9f
Gabriel Krenkel <gabrielkrenkel6@gmail.com>
2021-09-08 17:20:47 -0300

    update

--- Aula 74/jogo/src/components/Square/index.jsx
+++ Aula 74/jogo/src/components/Square/index.jsx
@@ -1,22 +1,11 @@
-import React from 'react'
-
-export class Square extends React.Component {
-    constructor(props){
-      super(props)
-
-      this.state = {
-        value: null,
-      };
-    }
-
-    render() {
-      return (
-        <button 
-            className="square" 
-            onClick={() => { this.setState({ value: "X"})}}
-          >
-            {this.state.value}
-        </button>
-      );
-    }
-  }
+export function Square(props) {    
+  return (
+    <button 
+        className="square" 
+        onClick={() => { props.onClick() }}
+      >
+        {props.value}
+    </button>
+  )
+}
+  
---
+++ Aula 78/exercicio/src/App.js
@@ -0,0 +1,11 @@
+const { navBar } = require("./components/Menu/menu");
+
+function App() {
+  return (
+    <div>
+      <navBar/>
+    </div>
+  );
+}
+
+export default App;
